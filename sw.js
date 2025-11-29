d3e07574188ae2483a265309774e59437bbfe3f9
l0ntra <105969104+Vydritskiy@users.noreply.github.com>
2025-11-29 20:40:16 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-// basic sw
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  // very small fetch handler: try network first, fall back to cache if offline
+  event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));
+});
